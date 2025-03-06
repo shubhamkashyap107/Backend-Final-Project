e5073c9f026439cf632b07a799ff19c1fead9789
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-03-06 16:13:53 +0530

    Set Up the folder and make User schema

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+require('dotenv').config()
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server connected to port " + process.env.PORT)
+})
---
+++ src/models/userSchema.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose")
+var validator = require('validator');
+
+
+const userSchema = new mongoose.Schema({
+    firstName : {
+        type : String,
+        required : true,
+        trim : true,
+        minLength : 2,
+        maxLength : 15
+    },
+    lastName : {
+        type : String,
+        // required : true,
+        trim : true,
+        minLength : 2,
+        maxLength : 15
+    },
+    username : {
+        type : String,
+        required : true,
+        trim : true,
+        unique : true,
+        minLength : 1,
+        maxLength : 15,
+        lowercase : true
+    },
+    email : {
+        type : String,
+        required : true,
+        trim : true,
+        unique : true,
+        minLength : 11,
+        lowercase : true,
+        validate(value){
+            const flag = validator.isEmail(value)
+            if(!flag)
+            {
+                throw new Error("Please enter a valid email")
+            }
+        }
+    },
+    password : {
+        type : String,
+        required : true,
+        trim : true
+    },
+
+    dateOfBirth : {
+        type : String,
+        required : true,
+        trim : true,
+        validate(value){
+            const flag = validator.isDate(value)
+            if(!flag)
+            {
+                throw new Error("Please enter a valid date")
+            }
+        }
+    }
+})
+
+
+
+const User = mongoose.model("User", userSchema)
+
+
+module.exports = {
+    User
+}
+
